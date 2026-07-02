658a16d79ed44b09d4fb693b49fe48b9da25f1db
rohan2-4 <rohangadade24@gmail.com>
2026-07-02 10:14:19 +0530

    Setup Tailwind CSS and configure project styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 function Home() {
     return (
-        <div>
-            <h1>Welcome to the Datasight AI </h1>
-            <p>Your Business Intelligence Dashboard</p>
+        <div className = "min-h-screen bg-slate-100 flex items-center justify-center ">
+        <div className = "text-center">
+        
+            <h1 className="text-6xl font-bold text-blue-600">Datasight AI </h1>
+            <p className="text-gray-600 text-xl mt-4">Business Intelligence Dashboard</p>
+        </div>
         </div>
     );
 }
